6761f58a003465bb13ede7ed0b69482804da0b08
Hedy <hedywoo@gmail.com>
2016-04-30 15:33:02 -0600

    Finish api routig and api contraints

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ gem "active_model_serializers"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'ffaker'
+
 end
 
 group :development do
@@ -46,5 +50,6 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,20 @@ Rails.application.routes.draw do
 
   #Api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: "api" }, path: "/" do
+    scope module: :v1 do
+      #list the resources here
 
-    #list the resources here
-
+    end
   end
 end
 
 
 #rails can handle up to 21 different media types, you can list them by accessing the SET class under the MIME module.
+#
 #the JSON is one of the built0oin MIME types accepted by Rails, so we need to spicify this format as the default one
+#
 #up to this point we have not made anything crazy, what we want to achieve next is how to generate a base_uri under a subdomain, in our case something like api.market_place_api.dev.
+#
 #Setting the api under a subdomain is a good practie because it allows to escalate the applicatio to a DNS level.
 #
+#didn't just add a constrainst hash to spicity the subdomain, but also add the path option, and set it a backslash. this is telling rails to set the starting path for each request to be root in relation to the subdomain.
